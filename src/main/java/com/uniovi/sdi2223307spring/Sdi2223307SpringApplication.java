ec5875e7c77be02b1cb6c2f0bcb4c6c5c35b96a1
IkerJ14 <ikerjimenezh2@gmail.com>
2023-02-16 13:22:22 +0100

    Initial commit

---
+++ src/main/java/com/uniovi/sdi2223307spring/Sdi2223307SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223307spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223307SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223307SpringApplication.class, args);
+    }
+
+}
