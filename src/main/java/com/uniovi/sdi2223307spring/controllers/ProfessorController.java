78183e88c0b15515b2053706636e3f2eb01d6f63
IkerJ14 <ikerjimenezh2@gmail.com>
2023-02-17 11:46:28 +0100

    SDI-2223-307-sesión2-Ejercicio-complementario2

--- src/main/java/com/uniovi/sdi2223307spring/controllers/ProfessorController.java
+++ src/main/java/com/uniovi/sdi2223307spring/controllers/ProfessorController.java
@@ -3,6 +3,7 @@ package com.uniovi.sdi2223307spring.controllers;
 import com.uniovi.sdi2223307spring.entities.*;
 import com.uniovi.sdi2223307spring.services.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +25,14 @@ public class ProfessorController {
         professorsService.addProfessor(prof);
         return "redirect:/professor/list";
     }
+    @RequestMapping(value = "/professor/add")
+    public String getProfessor() {
+        return "professor/add";
+    }
     @RequestMapping("/professor/details/{dni}")
-    public String getDetail(@PathVariable Long dni) {
-        return professorsService.getProfessor(dni).toString();
+    public String getDetail(Model model,@PathVariable Long dni) {
+        model.addAttribute("professor", professorsService.getProfessor(dni));
+        return "professor/details";
     }
 
     @RequestMapping("/professor/delete/{dni}")
